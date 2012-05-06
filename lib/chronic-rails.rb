d5f4e29e646d3db03d147e955ff9921184ba913d
allen13 <kex.allen13@gmail.com>
2012-05-06 08:13:59 -0400

    Updated to convert to convert to date and made a separate method for time

--- lib/chronic-rails.rb
+++ lib/chronic-rails.rb
@@ -2,15 +2,21 @@ require 'chronic'
 
 class Class  
    def chronic_date(attr_name)  
-      attr_name = attr_name.to_s         # make sure it's a string       
+      chronic_attr(attr_name,:date) 
+   end
+   def chronic_time(attr_name)  
+     chronic_attr(attr_name)
+   end
+   def chronic_attr(attr_name,type=:time)
+     attr_name = attr_name.to_s       
       class_eval %Q(
         def #{attr_name}=(val)  
           if val.class == String
-            self[:#{attr_name}] = Chronic.parse(val)
+            self[:#{attr_name}] = Chronic.parse(val).to_#{type.to_s}
           else
             self[:#{attr_name}] = val
           end
         end  
-      ) 
-   end  
+      )
+   end
 end
--- spec/dummy/spec/models/person_spec.rb
+++ spec/dummy/spec/models/person_spec.rb
@@ -2,7 +2,7 @@ require File.expand_path("../../spec_helper", __FILE__)
 
 describe Person do
   it "should parse incoming strings with chronic" do
-    today = Chronic.parse "today"
+    today = Date.today
     p = Person.new
     p.birth = "today"
     p.birth.should == today
